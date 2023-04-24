8920126f277572bb48ff83ab8fd51ca735456190
Frank Liu <fliu@proserveit.com>
2023-04-23 22:31:28 -0400

    7. Routing in Web API

--- WebAPIDemo/Controllers/ShirtsController.cs
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -4,28 +4,34 @@ namespace WebAPIDemo.Controllers
 {
 
     [ApiController]
+    [Route("api/[controller]")]
     public class ShirtsController: ControllerBase
     {
+        [HttpGet]        
         public string GetShirts()
         {
             return "Reading all the shirts";
         }
 
+        [HttpGet("{id}")]        
         public string GetShirtById(int id)
         {
             return $"Reading shirt: {id}";
         }
 
+        [HttpPost]        
         public string CreateShirt()
         {
             return $"Creating a shirt";
         }
 
+        [HttpPut("{id}")]        
         public string UpdateShirt(int id)
         {
             return $"Updating shirt: {id}";
         }
 
+        [HttpDelete("{id}")]        
         public string DeleteShirt(int id)
         {
             return $"Deleting shirt: {id}";
--- WebAPIDemo/Program.cs
+++ WebAPIDemo/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -10,31 +11,7 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-// "/shirts"
-app.MapGet("/shirts", () =>
-{
-    return "Reading all the shirts";
-});
-
-app.MapGet("/shirts/{id}", (int id) =>
-{
-    return $"Reading shirt with ID: {id}";
-});
-
-app.MapPost("/shirts", () =>
-{
-    return "Creating a shirt.";
-});
-
-app.MapPut("/shirts/{id}", (int id) =>
-{
-    return $"Updating shirt with ID: {id}";
-});
-
-app.MapDelete("/shirts/{id}", (int id) =>
-{
-    return $"Deleting shirt with ID: {id}";
-});
+app.MapControllers();
 
 app.Run();
 
