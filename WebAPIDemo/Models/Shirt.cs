af8992f76a1bf88d406be322f5cb6f80695788be
Frank Liu <fliu@proserveit.com>
2023-05-03 13:21:53 -0400

    9. Model Validation with DataAnnatation

--- WebAPIDemo/Models/Shirt.cs
+++ WebAPIDemo/Models/Shirt.cs
@@ -1,12 +1,22 @@
-﻿namespace WebAPIDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIDemo.Models
 {
     public class Shirt
     {
         public int ShirtId { get; set; }
+
+        [Required]
         public string? Brand { get; set; }
+
+        [Required]
         public string? Color { get; set; }
-        public int Size { get; set; }
+
+        public int? Size { get; set; }
+
+        [Required]
         public string? Gender { get; set; }
-        public double Price { get; set; }
+
+        public double? Price { get; set; }
     }
 }
