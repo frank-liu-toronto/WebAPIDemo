dcba36757e9b106c1ce33b625c01b4a0e9a041d5
Frank Liu <fliu@proserveit.com>
2023-05-16 19:02:17 -0400

    13. ModelValidation with Action Filter

--- WebAPIDemo/Controllers/ShirtsController.cs
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIDemo.Filters;
 using WebAPIDemo.Models;
 using WebAPIDemo.Models.Repositories;
 
@@ -15,17 +16,11 @@ namespace WebAPIDemo.Controllers
             return Ok("Reading all the shirts");
         }
 
-        [HttpGet("{id}")]        
+        [HttpGet("{id}")]
+        [Shirt_ValidateShirtIdFilter]
         public IActionResult GetShirtById(int id)
         {
-            if (id <= 0)
-                return BadRequest();
-
-            var shirt = ShirtRepository.GetShirtById(id);
-            if (shirt == null)
-                return NotFound();
-
-            return Ok(shirt);
+            return Ok(ShirtRepository.GetShirtById(id));
         }
 
         [HttpPost]        
---
+++ WebAPIDemo/Filters/Shirt_ValidateShirtIdFilterAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using WebAPIDemo.Models.Repositories;
+
+namespace WebAPIDemo.Filters
+{
+    public class Shirt_ValidateShirtIdFilterAttribute: ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            var shirtId = context.ActionArguments["id"] as int?;
+            if (shirtId.HasValue)
+            {
+                if (shirtId.Value <= 0)
+                {
+                    context.ModelState.AddModelError("ShirtId", "ShirtId is invalid.");
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Status = StatusCodes.Status400BadRequest
+                    };
+                    context.Result = new BadRequestObjectResult(problemDetails);
+                }          
+                else if (!ShirtRepository.ShirtExists(shirtId.Value))
+                {
+                    context.ModelState.AddModelError("ShirtId", "Shirt doesn't exist.");
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Status = StatusCodes.Status404NotFound
+                    };
+                    context.Result = new NotFoundObjectResult(problemDetails);
+                }
+            }
+        }
+    }
+}
