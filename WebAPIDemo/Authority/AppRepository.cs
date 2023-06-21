6c0f6a97654fc2a75ebca1cac420678dba42168e
Frank Liu <fliu@proserveit.com>
2023-06-21 08:45:17 -0700

    44. Register apps against Authority

---
+++ WebAPIDemo/Authority/AppRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace WebAPIDemo.Authority
+{
+    public static class AppRepository
+    {
+        private static List<Application> _applications = new List<Application>()
+        {
+            new Application
+            {
+                ApplicationId = 1,
+                ApplicationName = "MVCWebApp",
+                ClientId = "53D3C1E6-4587-4AD5-8C6E-A8E4BD59940E",
+                Secret = "0673FC70-0514-4011-B4A3-DF9BC03201BC"
+            }
+        };
+
+        public static bool Authenticate(string clientId, string secret)
+        {
+            return _applications.Any(x => x.ClientId == clientId && x.Secret == secret);
+        }
+
+        public static Application? GetApplicationByClientId(string clientId)
+        {
+            return _applications.FirstOrDefault(x => x.ClientId == clientId);
+        }
+    }
+}
---
+++ WebAPIDemo/Authority/Application.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPIDemo.Authority
+{
+    public class Application
+    {
+        public int ApplicationId { get; set; }
+        public string? ApplicationName { get; set; }
+        public string? ClientId { get; set; }
+        public string? Secret { get; set; }
+    }
+}
