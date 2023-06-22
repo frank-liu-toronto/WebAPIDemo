e43cb42bc3b678130b01f8d55c1f7a2464e3ed90
Frank Liu <fliu@proserveit.com>
2023-06-21 21:55:51 -0700

    45. Authenticate apps against Authority

---
+++ WebAPIDemo/Controllers/AuthorityController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIDemo.Authority;
+
+namespace WebAPIDemo.Controllers
+{
+    [ApiController]
+    public class AuthorityController: ControllerBase
+    {
+        [HttpPost("auth")]
+        public IActionResult Authenticate([FromBody]AppCredential credential)
+        {
+            if (AppRepository.Authenticate(credential.ClientId, credential.Secret))
+            {
+                return Ok(new
+                {
+                    access_toke = CreateToken(credential.ClientId),
+                    expires_at = DateTime.UtcNow.AddMinutes(10)
+                }); 
+            }
+            else
+            {
+                ModelState.AddModelError("Unauthorized", "You are not authorized.");
+                var problemDetails = new ValidationProblemDetails(ModelState)
+                {
+                    Status = StatusCodes.Status401Unauthorized
+                };
+                return new UnauthorizedObjectResult(problemDetails);
+            }
+        }
+
+        private string CreateToken(string clientId)
+        {
+            return string.Empty;
+        }
+    }
+}
