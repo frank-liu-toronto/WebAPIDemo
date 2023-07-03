52aaa76213e298570fcfe0877551e70c719d65fc
Frank Liu <fliu@proserveit.com>
2023-07-03 15:43:17 -0400

    54. Swagger Document Support

--- WebAPIDemo/Program.cs
+++ WebAPIDemo/Program.cs
@@ -12,9 +12,17 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
