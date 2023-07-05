799e4ae3f2246d8b16666e1d03372abbc47116b1
Frank Liu <fliu@proserveit.com>
2023-07-04 21:24:48 -0400

    59. Run migration to add new field

--- WebAPIDemo/Migrations/20230705012207_AddDescriptionToShirt.Designer.cs
+++ WebAPIDemo/Migrations/20230705012207_AddDescriptionToShirt.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAPIDemo.Data;
 
@@ -11,9 +12,11 @@ using WebAPIDemo.Data;
 namespace WebAPIDemo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230705012207_AddDescriptionToShirt")]
+    partial class AddDescriptionToShirt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebAPIDemo/Migrations/20230705012207_AddDescriptionToShirt.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPIDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToShirt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Shirts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Shirts",
+                keyColumn: "ShirtId",
+                keyValue: 1,
+                column: "Description",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Shirts",
+                keyColumn: "ShirtId",
+                keyValue: 2,
+                column: "Description",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Shirts",
+                keyColumn: "ShirtId",
+                keyValue: 3,
+                column: "Description",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Shirts",
+                keyColumn: "ShirtId",
+                keyValue: 4,
+                column: "Description",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Shirts");
+        }
+    }
+}
